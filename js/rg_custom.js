ca47c7a5abd13440337d5f83fbbe48e1e8d37179
Jackson Lynch <jacksonthatsme@gmail.com>
2013-07-31 15:39:05 -0400

    Continued hammering out html and css

--- js/rg_custom.js
+++ js/rg_custom.js
@@ -13,5 +13,54 @@ $('.big-list > dt > a').click(function() {
     return false;
 });
 
-// CAROUSEL FUNCTIONALITY
+// CAROUSEL INITIAL POSITIONING
+
+$(window).load(function(){
+  //Declaring Variables
+  var ww = $(window).width();
+  var howmany = $('.collection').find('li').length;
+  
+  //Setting the WIDTH of the li
+  $('.collection li').css('width', Math.floor(((ww/30)*10)));
+  
+  //Setting the HEIGHT of the li
+  $('.collection li').css('height', Math.floor(((ww/30)*10)));
+  
+  //Declaring Variables
+  var liW = $('.collection li').width();
+  var liH = $('.collection li').height();
+  
+  //Setting the HEIGHT of the Container
+  $('.l-collection').css('height', ((ww/30)*10));
+  
+  //Setting the size of the #Collection
+  $('.collection').width((liW * howmany)+(howmany * 10));
+  
+  // Adding display block on load to list elements
+  $('.collection li').css('display', 'block');
+  
+});
+  
+//Maintaining Heights and Widths on resize
+$(window).resize(function(){
+  //Declaring Variables
+  var ww = $(window).width();
+  var howmany = $('.collection').find('li').length;
+  
+  //Setting the WIDTH of the li
+  $('.collection li').css('width', Math.floor(((ww/30)*10)));
+  
+  //Setting the HEIGHT of the li
+  $('.collection li').css('height', Math.floor(((ww/30)*10)));
+  
+  var liW = $('.collection li').width();
+  var liH = $('.collection li').height();
+  
+  //Setting the HEIGHT of the Container
+  $('.l-collection').css('height', ((ww/30)*10));
+  
+  //Setting the size of the #Collection
+  $('.collection').width(liW * howmany);
+  
+});
 
